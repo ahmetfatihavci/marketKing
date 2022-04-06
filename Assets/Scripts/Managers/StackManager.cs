5ce19055b1b8969231cee486af1954b6c66d8a15
Ahmet Fatih AVCI <ahmetfatihavcii@gmail.com>
2022-04-06 14:59:26 +0300

    StackManager update
    Added GiveProductToPlace and rename StackProduct

--- Assets/Scripts/Managers/StackManager.cs
+++ Assets/Scripts/Managers/StackManager.cs
@@ -4,21 +4,61 @@ using System.Collections.Generic;
 using DG.Tweening;
 using UnityEngine;
 
-public class StackManager : Singleton<StackManager>
+namespace Managers
 {
-
-    public void StackProduct(GameObject _pickerGameObject, Product _pickedProduct, Action<Product> Success)
+    public class StackManager : Singleton<StackManager>
     {
-       _pickedProduct.RemoveProductFromProducer?.Invoke(_pickedProduct.gameObject);
-        CharacterController _characterController = _pickerGameObject.GetComponent<CharacterController>();
-        
-        _pickedProduct.transform.parent = _pickerGameObject.transform;
-        _pickedProduct.transform.DOLocalMove(_characterController.lastStackTransform.localPosition, 0.3f);
-        Vector3 desPos = _characterController.lastStackTransform.localPosition;
-        desPos.y += 0.3f;
-        _characterController.lastStackTransform.localPosition = desPos;
-        Success?.Invoke(_pickedProduct);
+        public void StackProduct(CharacterController _picker, Product _pickedProduct, Action<Product> Success)
+        {
+            _pickedProduct.RemoveProductFromProducer?.Invoke(_pickedProduct.gameObject);
+            Vector3 _desPos = _picker.lastStackPosition;
+            _desPos.x = 0;
+            if (_picker.HasProduct)
+            {
+                _desPos.y += _pickedProduct.transform.localScale.y;
+            }
+
+            _desPos.z = 0;
+            _pickedProduct.transform.parent = _picker.stackTransform;
+            _pickedProduct.transform.DOLocalMove(_desPos, 0.3f);
+
+            _picker.lastStackPosition = _desPos;
+            Success?.Invoke(_pickedProduct);
+        }
+
+        public void GiveProductToPlace(CharacterController _giver, Place _place, Action<Product> Success)
+        {
+            Product willGiveProduct = _giver.GetProductByType(_place.productType);
+            if (willGiveProduct != null)
+            {
+                Vector3 _desPos = _giver.lastStackPosition;
+                _desPos.x = 0;
+                _desPos.y -= willGiveProduct.transform.localScale.y;
+                _desPos.z = 0;
+
+                willGiveProduct.transform.parent = _place.transform;
+                willGiveProduct.transform.DOLocalMove(new Vector3(0, 1.5f, 0), 0.3f);
+                Success?.Invoke(willGiveProduct);
+                _giver.lastStackPosition = _desPos;
+                SortProducts(_giver);
+            }
+            else
+            {
+                LogManager.Log("willGiveProduct is null",LogType.Warning);
+            }
+        }
+
+        public void SortProducts(CharacterController _characterController)
+        {
+            Vector3 _startPos = _characterController.stackTransform.localPosition;
+            for (int i = 0; i < _characterController.products.Count; i++)
+            {
+                Vector3 tmp = _startPos;
+                tmp.x = 0;
+                tmp.y = (i * _characterController.products[i].transform.localScale.y);
+                tmp.z = 0;
+                _characterController.products[i].transform.localPosition = tmp;
+            }
+        }
     }
-    
-    
 }
